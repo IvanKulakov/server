bb75175949cc9fe5d1264980e322b435145414d9
unknown <vn8007@gmail.com>
2021-11-16 18:19:15 +0200

    url

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://tests_db_user:12345qwerty@cluster0.fzpdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'mongodb+srv://TheBest:MJGiQK8BtmuEC6i@cluster0.5qewy.mongodb.net/madebyhands?retryWrites=true&w=majority',
   secretOrKey: 'random very very secret string',
 };
