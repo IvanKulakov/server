4c7fc279b3bb64dc410c16df6c699f69ceb14047
unknown <vn8007@gmail.com>
2021-11-16 16:42:26 +0200

    server

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://tests_db_user:qwerty12345@cluster0.fzpdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'mongodb+srv://tests_db_user:12345qwerty@cluster0.fzpdq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   secretOrKey: 'random very very secret string',
 };
